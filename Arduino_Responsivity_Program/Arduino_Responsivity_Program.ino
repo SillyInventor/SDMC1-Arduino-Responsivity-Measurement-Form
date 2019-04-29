1d278c4f7cf74d0d51f60441e2cb38e6d8e6ec5c
Silly Inventor <jamesdavis3004@gmail.com>
2019-04-29 11:10:15 -0400

    Adding averaging and fixing port set on Arduino

--- Arduino_Responsivity_Program/Arduino_Responsivity_Program.ino
+++ Arduino_Responsivity_Program/Arduino_Responsivity_Program.ino
@@ -5,20 +5,47 @@
 /////////////////////////////////////////////
 
 String receivedString = "";
+
 int port = A0;
+int lengthOfReading = 500;
+
+unsigned long readTime = 0;
+
+double finalReading = 0;
+double numberOfReadings = 0;
+double reading = 0;
+
+boolean takeReading = false;
+
 void setup() {
   Serial.begin(9600);
-  delay(1000);
-  Serial.print("ready");
+  delay(100);
+  Serial.println("ready");
 }
 
 void loop() {
+  // Have you completed a reading? If so, then send the reading
+  if(takeReading && millis()>readTime) {
+    finalReading = reading / numberOfReadings;
+    numberOfReadings = 0;
+    reading = 0;
+    takeReading = false;
+    Serial.println(finalReading);
+  }
+  // Are you still collecting a reading?
+  else if(takeReading) {
+    reading += analogRead(port);
+    numberOfReadings++;
+  }
+  // Are you being told to do something?
   if(Serial.available()) {
     receivedString = Serial.readStringUntil('\n');
-    if (receivedString == "read") {
-      Serial.println(analogRead(port));
+    if (receivedString == "read") { // Start reading
+      readTime = millis() + lengthOfReading;
+      takeReading = true;
     }
-    if (receivedString.indexOf("port=")>0){
+    if (receivedString.indexOf("port=")>=0){ // Set your port number
+      Serial.println(receivedString.substring(receivedString.indexOf("port=")+5));
       port = receivedString.substring(receivedString.indexOf("port=")+5).toInt();
       Serial.println("port set to " + port);
     }
